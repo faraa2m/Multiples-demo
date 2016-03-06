d300fd48e886780d559e38dd99e7a76a365e704d
mohdfaraaz1 <fmohamme@syr.edu>
2016-03-06 13:51:10 -0500

    Initial Commit

--- Multiples-demo/ViewController.swift
+++ Multiples-demo/ViewController.swift
@@ -10,16 +10,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //Outlets
+    @IBOutlet weak var logo:UIImageView!
+    @IBOutlet weak var multipleTxt:UITextField!
+    @IBOutlet weak var playBtn:UIButton!
+    
+    @IBOutlet weak var result: UILabel!
+    @IBOutlet weak var addBtn: UIButton!
+    
+    //Properties
+    var number:Int = 0
+    var prevSum:Int = 0
+    var curSum:Int = 0
+    var maxSum:Int = 100
+    
+    @IBAction func playOnBtn(sender:UIButton!){
+        if multipleTxt.text != nil && multipleTxt.text != ""{
+            logo.hidden = true
+            multipleTxt.hidden = true
+            playBtn.hidden = true
+            
+            result.hidden = false
+            addBtn.hidden = false
+            
+            prevSum = 0
+            number = Int(multipleTxt.text!)!
+            curSum = number
+            
+            displayResult()
+            
+        }
+    
+    }
+    
+    @IBAction func addPress(sender: UIButton!){
+        updateSums();
+        displayResult();
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func displayResult(){
+        result.text = " \(prevSum) + \(number) = \(curSum)"
     }
-
+    
+    func updateSums(){
+        prevSum = curSum
+        curSum += number
+        
+        if curSum > maxSum{
+            restart()
+        }
+    }
+    
+    func restart(){
+        
+        number = 0
+        multipleTxt.text = ""
+        
+        logo.hidden = false
+        multipleTxt.hidden = false
+        playBtn.hidden = false
+        
+        result.hidden = true
+        addBtn.hidden = true
+        
+    }
 
 }
 
